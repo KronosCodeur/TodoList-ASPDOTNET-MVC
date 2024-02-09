a5428bc2949dab3eb05ad4a054a970e9a166a92d
KronosCodeur <piraliarris@gmail.com>
2024-02-09 18:11:54 +0000

    Todo Controller

---
+++ TodoList/Controllers/TodoController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TodoList.Data;
+using TodoList.Models;
+
+namespace TodoList.Controllers;
+
+public class TodoController : Controller
+{
+    private readonly DatabaseContext _database;
+
+    public TodoController(DatabaseContext database)
+    {
+        _database = database;
+    }
+    
+    public IActionResult CreateTodo()
+    {
+        //ViewBag.Users = _database.Users.ToList();
+        ViewData["UserId"] = new SelectList(_database.Users, "Id", "Name");
+        return View();
+    }
+
+    public IActionResult ListTodos()
+    {
+        return View(_database.Todos.Include(t => t.User).ToList());
+    }
+
+    [HttpPost]
+    public IActionResult CreateTodo(Todo todo)
+    {
+            _database.Todos.Add(todo);
+            _database.SaveChanges();
+        ViewBag.Users = _database.Users.ToList();
+            return RedirectToAction("ListTodos");
+    }
+    
+}
