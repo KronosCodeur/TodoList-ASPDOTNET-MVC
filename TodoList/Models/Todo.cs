787f0cc360e700ec7b01321f544c6ffdef528183
KronosCodeur <piraliarris@gmail.com>
2024-02-10 19:34:04 +0000

    adding category

--- TodoList/Models/Todo.cs
+++ TodoList/Models/Todo.cs
@@ -7,6 +7,9 @@ public class Todo
 {
     [Key] public int Id { get; set; }
     public required string Title { get; set; }
+    [ForeignKey("CategoryId")]
+    public int CategoryId { get; set; }
+    public required Category Category { get; set; }
     [ForeignKey("UserId")]
     public int UserId { get; set; }
     public required User User { get; set; }
